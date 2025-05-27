05149aede93aacb142d04452ae2cf984a1808672
Conut-1 <1mim1@naver.com>
2025-05-27 16:41:50 +0900

    status 속성 제외

--- backend/src/main/java/org/kosa/board/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/kosa/board/exception/GlobalExceptionHandler.java
@@ -15,7 +15,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, Object>> handleNotFoundException(ResponseStatusException e) {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", e.getStatusCode().value());
         map.put("message", e.getReason());
         return ResponseEntity.status(e.getStatusCode()).body(map);
     }
@@ -23,7 +22,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidException(MethodArgumentNotValidException e) {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", HttpStatus.BAD_REQUEST.value());
         map.put("message", "Invalid Request");
         map.put("errors", e.getFieldErrors().stream().map((err) -> err.getField() + ": " + err.getDefaultMessage()).toList());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
