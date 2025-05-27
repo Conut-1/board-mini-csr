a8f8eabb87397194df28e63f99f72a1dfa57e8c7
Conut-1 <1mim1@naver.com>
2025-05-27 15:31:06 +0900

    커스텀 UserDetails 사용

---
+++ backend/src/main/java/org/kosa/board/auth/CustomUserDetails.java
@@ -0,0 +1,44 @@
+package org.kosa.board.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.kosa.board.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+    private final String id;
+    private final String password;
+    private final List<GrantedAuthority> authorities = new ArrayList<>();
+
+    public CustomUserDetails(Member member) {
+        this.id = member.getId();
+        this.password = member.getPassword();
+        if (member.isAdminAuth()) {
+            this.authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        }
+        this.authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
--- backend/src/main/java/org/kosa/board/auth/MemberSecurityService.java
+++ backend/src/main/java/org/kosa/board/auth/MemberSecurityService.java
@@ -1,14 +1,9 @@
 package org.kosa.board.auth;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.kosa.board.member.Member;
 import org.kosa.board.member.MemberRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,17 +18,10 @@ public class MemberSecurityService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        Optional<Member> _member = this.memberRepository.findById(id);
-        if (_member.isEmpty()) {
+        Optional<Member> member = this.memberRepository.findById(id);
+        if (member.isEmpty()) {
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
         }
-        Member member = _member.get();
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (member.isAdminAuth()) {
-            authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
-        } else {
-            authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
-        }
-        return new User(member.getId(), member.getPassword(), authorities);
+        return new CustomUserDetails(member.get());
     }
 }
