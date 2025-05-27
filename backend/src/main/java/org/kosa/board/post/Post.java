f34d89d3debe6cd345fa565d4853d369454fb3e5
Conut-1 <1mim1@naver.com>
2025-05-27 09:23:46 +0900

    게시글 Entity 작성

---
+++ backend/src/main/java/org/kosa/board/post/Post.java
@@ -0,0 +1,46 @@
+package org.kosa.board.post;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postNo;
+
+    @Column(length = 20)
+	private String password;
+
+    @Column(length = 150)
+	private String title;
+
+    @Lob
+	private String content;
+
+	private int views;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+
+	private LocalDateTime deleteDate;
+}
