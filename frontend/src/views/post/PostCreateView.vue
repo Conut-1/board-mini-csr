043c53f2b6e437fda7b96d855771da7f9acf0291
Conut-1 <1mim1@naver.com>
2025-05-28 12:09:06 +0900

    글 작성 입력값 검증

--- frontend/src/views/post/PostCreateView.vue
+++ frontend/src/views/post/PostCreateView.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-header">게시물 작성</div>
             <div class="card-body">
-                <form @submit.prevent="createPost" id="create-post-form">
+                <form @submit.prevent="createPost">
                     <div class="mb-3">
                         <label for="create-post-title" class="form-label">제목</label>
                         <input type="text" v-model="post.title" class="form-control" id="create-post-title" placeholder="제목">
@@ -49,66 +49,32 @@
 
     async function createPost() {
         // TODO: 응답을 받아서 post deatil로 이동
-        const response = await axios.post('/api/post/create', post);
-        router.push({ name: 'postList' })
+        if (post.title.length === 0) {
+            alert("제목을 입력해주세요.");
+            // title.focus();
+            return;
+        }
+
+        if (post.password.length === 0) {
+            alert("비밀번호를 입력해주세요.");
+            // password.focus();
+            return;
+        }
+
+        if (post.content.length === 0) {
+            alert("내용을 입력해주세요.");
+            // content.focus();
+            return;
+        }
+
+        if (!confirm("글을 작성하시겠습니까?")) return;
+
+        try {
+            await axios.post('/api/post/create', post);
+            alert("글 작성이 성공했습니다.");
+            router.push({ name: 'postList' })
+        } catch (e) {
+            alert("글 작성이 실패했습니다.");
+        }
     }
-
-    // const createPostForm = document.body.querySelector("#create-post-form");
-
-    // createPostForm.addEventListener("submit", createPost);
-
-    // async function createPost(e) {
-    //     e.preventDefault();
-
-    //     const title = createPostForm.querySelector("#create-post-title");
-    //     const password = createPostForm.querySelector("#create-post-password");
-    //     const content = createPostForm.querySelector("#create-post-content");
-
-    //     if (title.value.length === 0) {
-    //         alert("제목을 입력해주세요.");
-    //         title.focus();
-    //         return;
-    //     }
-
-    //     // if (!category) {
-    //     //     alert("카테고리를 선택해주세요.");
-    //     //     form.category.focus();
-    //     //     return;
-    //     // }
-
-    //     const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+=-]).{8,20}$/;
-    //     if (!passwordPattern.test(password.value)) {
-    //         alert("비밀번호가 유효하지 않습니다.");
-    //         password.focus();
-    //         return;
-    //     }
-
-    //     if (content.value.length === 0) {
-    //         alert("내용을 입력해주세요.");
-    //         content.focus();
-    //         return;
-    //     }
-
-    //     if (!confirm("글을 작성하시겠습니까?")) return;
-
-    //     const body = {
-    //         title: title.value,
-    //         password: password.value,
-    //         content: content.value,
-    //     };
-
-    //     const response = await fetch("create", {
-    //         method: 'post',
-    //         headers: { 'Content-Type': 'application/json;charset=utf-8' },
-    //         body: JSON.stringify(body)
-    //     });
-
-    //     const json = await response.json();
-    //     if (json.status === "ok") {
-    //         alert("글 작성이 성공했습니다.");
-    //         location.href = 'list';
-    //         return;
-    //     }
-    //     alert("글 작성이 실패했습니다.");
-    // }
 </script>
