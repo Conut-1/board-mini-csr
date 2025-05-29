5e0bdda6e7ce5686a9de8736bab6e16e779ce165
Conut-1 <1mim1@naver.com>
2025-05-29 11:29:53 +0900

    로그인 API 변경에 맞춰서 수정

--- frontend/src/views/member/MemberLoginView.vue
+++ frontend/src/views/member/MemberLoginView.vue
@@ -3,10 +3,10 @@
         <div class="card p-4 w-100 mw-360px">
             <form @submit.prevent="login">
                 <div class="mb-3">
-                    <input type="text" v-model="formData.id" class="form-control" placeholder="아이디">
+                    <input type="text" v-model="member.id" class="form-control" placeholder="아이디">
                 </div>
                 <div class="mb-3">
-                    <input type="password" v-model="formData.password" class="form-control" placeholder="비밀번호">
+                    <input type="password" v-model="member.password" class="form-control" placeholder="비밀번호">
                 </div>
                 <div class="d-grid mb-3">
                     <input type="submit" class="btn text-white btn-primary" value="로그인"></input>
@@ -26,32 +26,28 @@
 
     const { fetchMe } = inject('actions');
 
-    const formData = reactive({ id: '', password: '' });
+    const member = reactive({ id: '', password: '' });
 
     async function login() {
         // TODO: focus 추가
-        if (!(8 <= formData.id.length && formData.id.length <= 20)) {
+        if (!(8 <= member.id.length && member.id.length <= 20)) {
             alert("아이디는 8자 이상 20자 이하로 입력하세요.");
             return;
         }
 
         // const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+=-]).{8,20}$/;
-        // if (!passwordPattern.test(formData.password)) {
-        if (formData.password.length === 0) {
+        // if (!passwordPattern.test(member.password)) {
+        if (member.password.length === 0) {
             alert("비밀번호가 유효하지 않습니다.");
             return;
         }
 
-        const params = new URLSearchParams();
-        params.append('username', formData.id);
-        params.append('password', formData.password);
-
         try {
-            await axios.post('/api/member/login', params);
+            await axios.post('/api/auth/login', member);
             await fetchMe();
             router.push({ name: 'home' });
         } catch (e) {
-            alert("로그인 실패");
+            alert(e.response.data.message);
         }
     }
 </script>
