ccdfa64d6dc9fb9ed8d8be024f810e00f2893ab2
Conut-1 <1mim1@naver.com>
2025-06-01 18:02:54 +0900

    TODO 업데이트

--- backend/src/main/java/org/kosa/board/BoardApplication.java
+++ backend/src/main/java/org/kosa/board/BoardApplication.java
@@ -14,11 +14,8 @@ public class BoardApplication {
 
 // TODO: 게시글 작성, 수정 시에 조회수 올림 X
 // TODO: 검증에 실패한 곳에 focus
-// TODO: invalid 시각화, 검증을 한 번에 띄우기
+// TODO: 살짝 검색결과 없음 뜨는 거 없애기 가능?
 
 // TODO: 트랜잭션 확인, OSIV?
-// TODO: Vue.js 빌드 설정
-
-// TODO: DB에서 가져오는 값 줄이기
 // TODO: axios 인스턴스로 처리?
 // TODO: CSRF 처리
